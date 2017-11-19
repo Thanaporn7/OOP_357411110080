7746bef33035d243fbce14c032cd0c925b40aa61
Thanaporn7 <thanapornarma@gmail.com>
2017-11-19 14:48:56 +0700

    Testing upload to github

---
+++ src/oop_lab/s357411110080/rmutsv/com/HelloWord.java
@@ -0,0 +1,14 @@
+package oop_lab.s357411110080.rmutsv.com;
+
+public class HelloWord {
+
+//  create main method
+public static void main(String[] args) {
+
+    System.out.println("Hello World,I'am Mew");
+
+
+}//main
+
+
+ }//class
