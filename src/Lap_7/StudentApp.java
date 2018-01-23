2678cf4312e5a1be44fe1551a16cda24c0a7389c
Thanaporn7 <thanapornarma@gmailc.om>
2018-01-23 09:56:07 +0700

    Class and object: student1

---
+++ src/Lap_7/StudentApp.java
@@ -0,0 +1,46 @@
+package Lap_7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        //create Objects as Student
+        student student1 = new student();
+
+        Student1.SetId("111111111111");
+        student1.setMajor("Information System");
+        student1.setFaculty("Management Technology");
+        student1.setName("Boy Saiyai");
+        system.out.println()
+
+        student1.toString(Student1.toString());
+
+        student student2 = new Student("222222222222",
+
+                m: "Management", f " Business Management",
+                n: "Girl Songkhla");
+
+        system.out.println(Studet2.toString());
+
+        syudent student3 = new Student();
+        syudent3 = inputData (student3 );
+
+        system.out.println(student3.toString());
+
+    }//main
+
+    private static student inputData( student s) {
+        scanner scanner = new scanner(system.in);
+        system.out.println("Enter your student data: ");
+                system.out.print("Name: ");
+        s.setName(scanner.nextLine());
+        system.out.print("ID: ");
+        s.set
+        system.out.print("Major: :");
+        s.setMajor(scanner.nextLine());
+        sysem.out.print("Faculty:");
+        s.setFaculty(scanner.nextLine));
+
+    retun s;
+
+    }
+}//class
+
