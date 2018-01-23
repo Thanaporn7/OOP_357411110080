6a7b6222d6b55fc0473a0f6c3ca5389a2fb6813c
Thanaporn7 <thanapornarma@gmailc.om>
2018-01-23 10:19:22 +0700

    Class and object: student

--- src/Lap_7/StudentApp.java
+++ src/Lap_7/StudentApp.java
@@ -1,45 +1,45 @@
 package Lap_7;
 
+import java.util.Scanner;
+
 public class StudentApp {
     public static void main(String[] args) {
         //create Objects as Student
         student student1 = new student();
 
-        Student1.SetId("111111111111");
+        student1.setId("111111111111");
         student1.setMajor("Information System");
         student1.setFaculty("Management Technology");
         student1.setName("Boy Saiyai");
-        system.out.println()
+        System.out.println();
 
-        student1.toString(Student1.toString());
+        student1.toString();
 
-        student student2 = new Student("222222222222",
 
-                m: "Management", f " Business Management",
-                n: "Girl Songkhla");
+        student student2 = new student("222222222222", "Management", " Business Management", "Girl Songkhla");
 
-        system.out.println(Studet2.toString());
+        System.out.println(student2.toString());
 
-        syudent student3 = new Student();
-        syudent3 = inputData (student3 );
+        student student3 = new student();
+        student3 = inputData (student3 );
 
-        system.out.println(student3.toString());
+        System.out.println(student3.toString());
+
 
     }//main
 
     private static student inputData( student s) {
-        scanner scanner = new scanner(system.in);
-        system.out.println("Enter your student data: ");
-                system.out.print("Name: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your student data: ");
+                System.out.print("Name: ");
         s.setName(scanner.nextLine());
-        system.out.print("ID: ");
-        s.set
-        system.out.print("Major: :");
+        System.out.print("ID: ");
+        s.setId(scanner.nextLine());
+        System.out.print("Major: :");
         s.setMajor(scanner.nextLine());
-        sysem.out.print("Faculty:");
-        s.setFaculty(scanner.nextLine));
-
-    retun s;
+        System.out.print("Faculty:");
+        s.setFaculty(scanner.nextLine());
+        return s;
 
     }
 }//class
