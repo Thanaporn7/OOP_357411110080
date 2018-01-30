ccdd018820ed9bc99e4ee33ff6a0950da891a4e3
Thanaporn7 <thanapornarma@gmail.com>
2018-01-30 10:30:03 +0700

    Person and Student

---
+++ src/OOp_lab3/TestForLoop.java
@@ -0,0 +1,21 @@
+package OOp_lab3;
+
+public class TestForLoop {
+    public static void main (String[] args) {
+
+//        for (int i=1;i<=10;i++)
+//            system.out.println("Hello.");
+//
+//        for (int i=1;i<=10;i++
+//        system.out.println(i);
+//         for (int i=1;i<=100;i++)
+              system.out.print(i+" ");
+        for (int i=1;i<=100;i++){
+            if (i%2==0)
+                system.out.print(i+ "");
+    }
+
+        for (int i=1;i<=100;i++){
+            if (i%3==0 && i%5==00)
+                system.out.print(i+ "");
+        }
---
+++ src/OOp_lab3/TestSwichCase.java
@@ -0,0 +1,23 @@
+package OOp_lab3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TestSwichCase {
+    public static void main (String [] args){
+        //       switch-case
+        BufferedReader reader =new BufferedRader() {
+            new InputStreamReader(System.in) ;
+            int input;
+        system.out.print("Plase  enter number (1-3):");
+            input = Integer.parseInt (reader.readLine ());
+           switch (input){
+                case  1: System.out.println ("Your entered 1."); break;
+                case  2: System.out.println ("Your entered 2."); break;
+                case  3: System.out println ("Your entered 3."); break;
+                default: System.out.printin("Please enter number 1-3.");
+
+            }//swith
+        }//main
+        lass
+
