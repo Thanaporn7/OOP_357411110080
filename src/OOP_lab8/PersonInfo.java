877d643b4c5f13a2002ee23aea0cd4e62b675ea7
Thanaporn7 <thanapornarma@gmail.com>
2018-01-30 10:40:36 +0700

    PersonInfo

---
+++ src/OOP_lab8/PersonInfo.java
@@ -0,0 +1,11 @@
+package OOP_lab8;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        student student = new student("1111111111111",
+                "Boy Saiyai","Male",20,"22222222222222","IS Manament Technology")
+                System.out.println(student.toString());
+                System.out.println(student.getName());
+
+    }
+}
